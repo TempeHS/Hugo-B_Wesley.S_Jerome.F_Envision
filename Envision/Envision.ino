bf567433a42545da8e6a1d2a80c96484b67c6ff6
Hugo B <hugo.brown8@education.nsw.gov.au>
2024-11-20 12:39:37 +1100

    Update Envision.ino

--- Envision/Envision.ino
+++ Envision/Envision.ino
@@ -1,4 +1,8 @@
 Jerome: https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue
 
 
-Wesley: htt
+Wesley: https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue
+
+
+Hugo: https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/tree/main/TempeHS_Sensor_Catalogue
+Ultrasonic distance sensor pin D7
