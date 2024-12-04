1061fb21327f4e1c6317cc90684a4f1c7b28c442
Wesley S <wesley.smith7@education.nsw.gov.au>
2024-12-04 11:48:03 +1100

    editing distance trigger

--- Envision/Envision.ino
+++ Envision/Envision.ino
@@ -1,13 +1,12 @@
 
 #include <Servo.h>
 
-// pins
 const int ultrasonicPin = 7;
 const int speakerPin = 6;
 
-// distance measurement
 long duration;
 int distance;
+
 
 void setup() {
 
@@ -17,7 +16,7 @@ void setup() {
 }
 
 void loop() {
-  // trigger the ultrasonic sensor
+
   pinMode(ultrasonicPin, OUTPUT); 
   digitalWrite(ultrasonicPin, LOW);
   delayMicroseconds(2);
@@ -25,18 +24,14 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(ultrasonicPin, LOW);
 
-  // Measure the echo time
   pinMode(ultrasonicPin, INPUT); 
   duration = pulseIn(ultrasonicPin, HIGH);
 
-  // distance in cm
   distance = duration * 0.035 / 2;
 
-  // distance to the Serial Monitor
   Serial.println(distance);
 
-  // sound if the object is closer than 1 metre
-  if (distance > 0 && distance < 150) { 
+  if (distance > 30 && distance < 150) { 
     tone(speakerPin, 1000, 100); 
   } else {
     noTone(speakerPin); 
